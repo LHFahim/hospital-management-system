26bb592dbd6b3a9ba7a31d234a48b85adb3a4841
Fahim-H <fahimlutful@gmail.com>
2022-10-28 14:43:41 +0600

    added ward section

--- src/pages/ViewWardDetails.js
+++ src/pages/ViewWardDetails.js
@@ -10,16 +10,16 @@ const ViewWardDetails = () => {
       <section className="py-10 w-5/6 mx-auto">
         <table>
           <thead className="space-x-10">
-            <th className="p-5">Room ID</th>
-            <th className="p-5">Room type</th>
-            <th className="p-5">Room rate</th>
+            <th className="p-5">Ward ID</th>
+            <th className="p-5">Ward type</th>
+            <th className="p-5">Ward rate</th>
             <th className="p-5">Notes</th>
           </thead>
           <tbody>
             <tr className=" border border-blue-900  hover:bg-blue-100">
               <td>1</td>
               <td>AC</td>
-              <td>1000 BDT</td>
+              <td>200 BDT</td>
               <td>Don't eat for 8 hours</td>
             </tr>
           </tbody>
